d75c45c9f6159750d5a48ed9a8f13961d3410b0e
Tomas Sykora, jr <jump@syky.cz>
2016-11-15 22:39:40 +0100

    TEST: Register

--- Tests/TitanTests/Droplet+Test.swift
+++ Tests/TitanTests/Droplet+Test.swift
@@ -9,45 +9,41 @@ import XCTest
 
 
 class HelloWorldTests: XCTestCase {
+    let drop = Droplet(arguments: ["dummy/path", "prepare"])
+    var token: String = ""
     
-    
-//    override func setUp() {
-//       
-//
-//        
-//        super.setUp()
-//        
-//        
-//    }
+    override func setUp() {
+        drop.database = Database(MemoryDriver())
+        try! load(drop)
+        try! drop.runCommands()
+        
+        super.setUp()
+    }
     
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
     
-    func testExample() throws {
-//        do {
-        let drop = Droplet()
-        drop.database = Database(MemoryDriver())
-//        try drop.runCommands()
-        
-        try load(drop)
+    func testRegister() throws {
         
         let registerRequest  = try Request(method: .post, uri: "/api/v1/users/register")
         registerRequest.headers = ["Content-Type" : "application/json"]
-        registerRequest.body = JSON(["username" : "username",
+        registerRequest.body = JSON(["username" : "syky",
                                      "password" : "password"]).makeBody()
         let registerResponse = try drop.respond(to: registerRequest)
+        XCTAssertNotNil(registerResponse.json?["token"]?.string)
+        token = (registerResponse.json?["token"]?.string)!
         
         XCTAssertEqual(200, registerResponse.status.hashValue)
         
-        print(registerResponse)
-//        } catch let e {
-//            print(e)
-//        }
-        
-        
     }
+    
+//    func testLogin() throws {
+//        let loginRequest
+//    }
+    
+    
     
     func testPerformanceExample() {
         // This is an example of a performance test case.
