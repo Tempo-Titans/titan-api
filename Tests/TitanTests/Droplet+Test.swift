483c8d43785250fc9ba69d23c5b46403404627e9
Tomas Sykora, jr <jump@syky.cz>
2016-11-16 00:03:31 +0100

    TEST: Delete user

--- Tests/TitanTests/Droplet+Test.swift
+++ Tests/TitanTests/Droplet+Test.swift
@@ -12,11 +12,8 @@ class TitanTests: XCTestCase {
     let drop = Droplet(arguments: ["dummy/path", "prepare"])
     var token: String = ""
     
-    
     override func setUp() {
-        
         drop.database = Database(MemoryDriver())
-//        try! drop.database?.delete("users")
         try! load(drop)
         try! drop.runCommands()
         
@@ -33,16 +30,75 @@ class TitanTests: XCTestCase {
         
         let registerResponse = try! drop.respond(to: registerRequest)
         XCTAssertNotNil(registerResponse.json)
+        
         if let token = registerResponse.json?["token"]?.string {
             self.token = token
         }
+    }
+    
+    func testGetUsers() throws {
+        try patchSykyToAdmin()
+        let usersRequest  = try Request(method: .get, uri: "/api/v1/users")
+        usersRequest.headers = ["Content-Type" : "application/json",
+                                "Authorization": "Bearer \(token)"]
+        
+        let userResponse = try drop.respond(to: usersRequest)
+        XCTAssertNotNil(userResponse.json)
+        
+        XCTAssertEqual(200, userResponse.status.hashValue)
+    }
+    
+    func testDeleteUser() throws {
+        try patchSykyToAdmin()
+        
+        let userToDeleteID = try registerUser(username: "fekal", password: "fekal")
+        
+        let deleteReq = try Request(method: .delete, uri: "/api/v1/users/\(userToDeleteID)")
+        deleteReq.headers = ["Content-Type" : "application/json",
+                             "Authorization": "Bearer \(token)"]
+        
+        let userResponse = try drop.respond(to: deleteReq)
+        XCTAssertEqual(200, userResponse.status.hashValue)
+        
+        let getDeleted = try Request(method: .get, uri: "/api/v1/users/\(userToDeleteID)")
+        getDeleted.headers = ["Content-Type" : "application/json",
+                             "Authorization": "Bearer \(token)"]
+        
+        let getDeletedRes = try drop.respond(to: getDeleted)
+        XCTAssertEqual(404, getDeletedRes.status.hashValue)
+        
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+}
+
+extension TitanTests {
+    func registerUser(username: String, password: String) throws -> Int{
+        let registerReq  = try! Request(method: .post, uri: "/api/v1/users/register")
+        registerReq.headers = ["Content-Type" : "application/json"]
+        registerReq.body = JSON(["username" : username.makeNode(),
+                                 "password" : password.makeNode()]).makeBody()
+        
+        let registerRes = try! drop.respond(to: registerReq)
+        XCTAssertEqual(200, registerRes.status.hashValue)
+        XCTAssertNotNil(registerRes.json)
+        if let id = registerRes.json?["id"]?.int {
+            return id
+        }
+        
+        XCTFail()
+        return 0
     }
     
     func patchSykyToAdmin() throws {
         let role = "admin"
         let patchReq = try Request(method: .patch, uri: "/api/v1/me")
         patchReq.headers = ["Content-Type" : "application/json",
-                                "Authorization": "Bearer \(token)"]
+                            "Authorization": "Bearer \(token)"]
         patchReq.body = JSON(["role" : role.makeNode()]).makeBody()
         
         let patchRes = try drop.respond(to: patchReq)
@@ -52,26 +108,4 @@ class TitanTests: XCTestCase {
         
         
     }
-    
-    func testGetUsers() throws {
-        try patchSykyToAdmin()
-        let usersRequest  = try Request(method: .get, uri: "/api/v1/users")
-        usersRequest.headers = ["Content-Type" : "application/json",
-                                "Authorization": "Bearer \(token)"]
-        
-        
-        let userResponse = try drop.respond(to: usersRequest)
-        XCTAssertNotNil(userResponse.json)
-        
-        
-        XCTAssertEqual(200, userResponse.status.hashValue)
-    }
-    
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
 }
-
