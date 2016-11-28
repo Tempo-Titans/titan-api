fdbcee57775354cc1c9e824977ff194ef199a05f
Tomas Sykora, jr <jump@syky.cz>
2016-11-28 21:27:49 +0100

    WIP: CSAS Integration

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "titan",
     dependencies: [
-        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1, minor: 1),
+        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1),
         .Package(url: "https://github.com/vapor/mysql-provider.git", majorVersion: 1)
         
     ],
